e3479094a020b206af99e968e2b4d1f8880204fe
André Machado <andrels.machado82@gmail.com>
2024-09-14 10:33:50 -0300

    Add classe de auditoria

---
+++ book-network-api/src/main/java/com/andre/book/config/ApplicationAuditAware.java
@@ -0,0 +1,22 @@
+package com.andre.book.config;
+
+import com.andre.book.model.User;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+        User userPrincipal = (User) authentication.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getId());
+    }
+}

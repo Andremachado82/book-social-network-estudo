91016f4521269c8bafd00dbb204815615a7dc7cb
André Machado <andrels.machado82@gmail.com>
2024-09-14 09:53:18 -0300

    Add entidades Book, Feedback e BaseEntity.

---
+++ book-network-api/src/main/java/com/andre/book/feedback/Feedback.java
@@ -0,0 +1,21 @@
+package com.andre.book.feedback;
+
+import com.andre.book.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    private Double note;
+    private String comment;
+}

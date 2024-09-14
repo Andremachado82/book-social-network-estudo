884054876eafd987e96d04152799457c70c813f1
André Machado <andrels.machado82@gmail.com>
2024-09-14 11:08:34 -0300

    Add método para salvar um Book.

--- book-network-api/src/main/java/com/andre/book/book/BookController.java
+++ book-network-api/src/main/java/com/andre/book/book/BookController.java
@@ -1,4 +1,25 @@
 package com.andre.book.book;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("books")
+@RequiredArgsConstructor
+@Tag(name = "Book")
 public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Integer>  saveBook(
+            @Valid @RequestBody BookRequest request,
+            Authentication connectedUser
+    ) {
+         return ResponseEntity.ok(bookService.saveBook(request, connectedUser));
+    }
 }

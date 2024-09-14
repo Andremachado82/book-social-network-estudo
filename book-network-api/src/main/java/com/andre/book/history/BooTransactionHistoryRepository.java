405392250ca926a8e549b3dcfc7c78e6ff3cb643
André Machado <andrels.machado82@gmail.com>
2024-09-14 14:20:25 -0300

    Add lógica para buscar lista de books devolvidos com paginação.

--- book-network-api/src/main/java/com/andre/book/history/BooTransactionHistoryRepository.java
+++ book-network-api/src/main/java/com/andre/book/history/BooTransactionHistoryRepository.java
@@ -13,4 +13,11 @@ public interface BooTransactionHistoryRepository extends JpaRepository<BookTrans
             WHERE history.user.id = :userId
             """)
     Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, Integer userId);
+
+    @Query("""
+            SELECT history
+            FROM BookTransactionHistory history
+            WHERE history.book.owner.id = :userId
+            """)
+    Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Integer userId);
 }
